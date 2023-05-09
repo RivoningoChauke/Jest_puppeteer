bc707042f33b96be15217122acb5b7168fca0ad8
Rivoningo Chauke <mashambarm26@gmail.com>
2023-05-09 14:23:00 +0200

    working on puppeteer

--- puppeteer_refresher.js
+++ puppeteer_refresher.js
@@ -7,5 +7,23 @@ const puppeteer = require("puppeteer");
         const browser = await puppeteer.launch({
             headless: false
         });
+
+        const page = await browser.newPage(); //it create new page
+
+        //go to the new page
+
+        await page.goto('https://google.com')
+
+        const searchSelector = 'input[title="Search"]'  //attribute with title search
+
+        await page.waitForSelector(searchSelector)
+
+        await page.click(searchSelector, {clickCount: 1})
+
+        await page.type(searchSelector, "Pizza restaurants")
+
+        await page.keyboard.press("Enter")
+
+        console.log("done with automation")
     }
-)
+)()
