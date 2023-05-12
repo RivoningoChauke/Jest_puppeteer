1a26dfff3c1576808c56f9d1b5bd5c381b4c97d0
Rivoningo Chauke <mashambarm26@gmail.com>
2023-05-12 10:36:59 +0200

    fixing another errors

--- tests/usercreation.test.js
+++ tests/usercreation.test.js
@@ -12,8 +12,6 @@ function delay(ms){
         }, ms)
     })
 
-    //bhkdfg vsdjsg gjhtjklfgv
-
     function getErrors(errorSelectors) {
         const errorList = document.querySelectorAll(errorSelectors)
         if (errorList.length > 0) {
@@ -41,15 +39,18 @@ beforeAll(async () => {
 describe('Testing User Creation Functionality', () => {
     const firstNameSelector = "#first_name"
     const lastNameSelector = "#last_name"
-    const ageSelector = "#age";
+    const ageInputSelector = "#age";
     const buttonSelector = ".InputCont button";
     const errorSelectors = ".error li"
     const pageTitle = "Awesome Website"
+    const userSelector = ".userlist ul .names"
+    const ageItemSelector = ".userlist ul .ageitem"
+
 
     test('page should have corrrect title', async () => {
         const title = await page.title(); //string
 
-        expect(title).toBe("Awesome Website")
+        expect(title).toBe(pageTitle)
     });
 
     test('Should throw three errors if no input has been filled', async () => {
@@ -62,9 +63,45 @@ describe('Testing User Creation Functionality', () => {
     });
 
     test('should only show 2 errors "last name" and "age"', async () => {
-        await page.type(firstNameSelector, "Evidence")
+        await page.type(firstNameSelector, "Rivoningo")
         await page.click(buttonSelector)
         const errorsReceived = await page.evaluate(getErrors, errorSelectors)
         expect(errorsReceived).toEqual(['Last Name is required', 'Age is required'])
     });
+
+    test("Successfully added a user", async () => {
+        await page.type(lastNameSelector, "Mashamba")
+        await page.type(ageInputSelector, "28");
+
+        await page.click(buttonSelector)
+
+        await delay(1000)
+        const errorsReceived =await page.evaluate(getErrors, errorSelectors)
+
+        expect(errorsReceived).toEqual([])
+
+        const userData = await page((selectors) => {
+            const {userSelector, ageItemSelector} = selectors;
+            const allAges = [...document.querySelectorAll(ageItemSelector)].map(element => element.textContent);
+            const allUsers = [...document.querySelectorAll(userSelector)].map(element => element.textContent);
+
+            return{
+                allAges,
+                allUsers
+            }
+        }, {
+            userSelector,
+            ageItemSelector
+        })
+
+        expect(userData).toEqual({
+            allAges: ["28"],
+            allUsers: ["Rivoningo Mashamba"]
+        })
+
+    })
+})
+
+afterAll(async () => { (): void | PromiseLike<void>
+    await browser.close()
 })
